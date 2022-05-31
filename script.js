bd269bb3fa206a618d4a20b0ac654e5254ac2f3e
alfredo-saenz <alfredosaenz104@gmail.com>
2022-05-31 11:33:42 -0500

    fix the contact section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// This is a function for a startup modal and when you click it hides!
+
 let modal = document.querySelector('.modal-btn')
 let modalContainer = document.querySelector('.hide')
 
@@ -6,4 +8,26 @@ modal.addEventListener('click',init)
 function init() {
     console.log('click')
     modalContainer.style.display = "none"
+}
+// let lastNameInput = document.getElementById('lname')
+// const nameInput = document.getElementById('fname')
+// let messageInput = document.getElementById('subject')
+
+// document
+// 	.querySelector('form.pure-form')
+// 	.addEventListener('submit', function (e) {
+// 		//prevent the normal submission of the form
+// 		e.preventDefault();
+
+// 		console.log(nameInput.value);
+// 	});
+
+
+
+let fNameEl = document.querySelector('#fname');
+let lNameEl = document.querySelector('#lname');
+let messageEl = document.querySelector('#subject');
+
+function send() {
+console.log(fNameEl.value, lNameEl.value, messageEl.value);
 }
